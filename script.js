4da66249dca3e56d6afcd486b194acfb2763c215
Dovoyan <70629154+Dovoyan@users.noreply.github.com>
2022-06-15 02:59:44 +0300

    320px

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let radioChange = (radio) =>{
-  document.querySelector()
-}
+let closeButton = document.querySelector('.mobil_close');
+
+closeButton.addEventListener('click', () => {
+  let mobilWindow = document.querySelector('.mobil_menu')
+  mobilWindow.classList.add('mobil_menu-display_none')
+})
+
+
+let openButton = document.querySelector('.mobil_menu_icon');
+
+openButton.addEventListener('click', () => {
+  let mobilWindow = document.querySelector('.mobil_menu')
+  mobilWindow.classList.remove('mobil_menu-display_none')
+})
